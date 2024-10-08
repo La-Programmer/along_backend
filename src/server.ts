5112900f8cc80c5d8502af25a7a40d6e9fd64fe2
Chinedum14 <chinedumnwankwo18@gmail.com>
2024-10-09 00:32:57 +0100

    Authentication

--- src/server.ts
+++ src/server.ts
@@ -8,6 +8,7 @@ app.get('/', (req, res) => {
   res.send("Hello, TypeScript + Node.js + Express!");
 });
 
+
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 })
