acd4da6dddf042787d4adf7416159dde25983d8b
La-Programmer <justinoghenekomeebedi@gmail.com>
2024-10-15 11:41:18 +0100

    Bind port to env variable

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,20 @@
 import express from 'express';
+import dotenv from "dotenv";
 import controllerRouting from './routes/index'
 import connectToDb from './config/database';
 const bodyParser = require("body-parser");
+
+dotenv.config();
 
 const app = express();
 const jsonParser = bodyParser.json();
 app.use(jsonParser);
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 controllerRouting(app);
 
 app.listen(port, () => {
   connectToDb();
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is up and running on port ${port}`);
 })
